e8acf2dd9074a0580677a9a134a94c36267f83ed
Ravi Shekhar <ravi.shekhar00@gmail.com>
2022-06-16 18:16:06 -0500

    Ignore SSL error

--- Demo/src/main/java/com/braintreepayments/popupbridge/demo/PopupActivity.java
+++ Demo/src/main/java/com/braintreepayments/popupbridge/demo/PopupActivity.java
@@ -2,12 +2,24 @@ package com.braintreepayments.popupbridge.demo;
 
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.net.http.SslError;
 import android.os.Bundle;
+import android.webkit.SslErrorHandler;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.braintreepayments.api.PopupBridgeClient;
+
+
+class SSLTolerentWebViewClient extends WebViewClient {
+
+    @Override
+    public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+        handler.proceed(); // Ignore SSL certificate errors
+    }
+}
 
 public class PopupActivity extends AppCompatActivity {
 
@@ -19,6 +31,10 @@ public class PopupActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_popup);
         mWebView = findViewById(R.id.web_view);
+
+        mWebView.setWebViewClient(
+                new SSLTolerentWebViewClient()
+        );
 
         mPopupBridgeClient = new PopupBridgeClient(this, mWebView, "com.braintreepayments.popupbridgeexample");
         mPopupBridgeClient.setErrorListener(error -> showDialog(error.getMessage()));
